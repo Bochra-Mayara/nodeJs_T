720a579e813152098bfc7a05d83e6b1d096f7e5d
bochra-mayara <bochra-mayara>
2022-10-15 22:51:23 +0200

    second commit

--- app.js
+++ app.js
@@ -1,16 +1,49 @@
-const _ =  require('lodash')
+
+const {readFile, writeFile} = require('fs').promises
+// const util = require ('util')
+// const readFilePromise = util.promisify(readFile)
+// const writeFilePromise = util.promisify(writeFile)
 
 
 
-const items = [1 , [2, [3 , [4]]]]
+// const getText = (path) => {
+//     return new Promise((resolve, reject)=> {
+//         readFile(path, 'utf8', (err, data) => {
+//             if(err) {
+//                reject(err)
+//             }else {
+//                 resolve(data)
+//             }
+//         })
+//     })
+// }
+/////Function async
+const start = async () => {
+    try {
+        const first = await readFile('./content/first.txt', 'utf8')
+        const second = await readFile('./content/second.txt', 'utf8')
+        await writeFile(
+            './content/result-mind.txt',
+            `THIS IS Awesome : ${first} ${second}`, 
+            {flag:'a'}
+        )
+        console.log(first, second)
+    } catch (error) {
+        console.log(error)
+        
+    }
+    
+   
+}
 
 
-const newItems = _.flattenDeep(items)
-
-console.log(newItems)
 
 
+start()
 
+// getText('../content/first.txt')
+// .then(result => console.log(result))
+// .catch(err => console.log(err))
 
 
 
