5bc749d87f1e622e1a0f35948d769b519c099564
bochra-mayara <bochra-mayara>
2022-10-19 11:36:24 +0200

    Feature

--- app.js
+++ app.js
@@ -35,13 +35,31 @@ app.get('/api/products/:productID', (req,res) => {
 app.get('/api/products/:productID/reviews/:reviewID' , (req,res) => {
     console.log(req.params)
     res.send('Hello World')
+  
 })
 
 
 app.get('/api/v1/query', (req,res) => {
  
-       console.log(req.query)
-        res.send('Hello World')
+    const { search, limit} = req.query
+    let sortedProducts = [...products]
+    if(search){
+        sortedProducts = sortedProducts.filter((product) =>{
+            return product.name.startsWith(search)
+        })
+    }
+
+    if(limit){
+        sortedProducts = sortedProducts.slice(0,Number(limit))
+    }
+
+    if (sortedProducts.length <1){
+        // res.status(200).send('No products matched your search')
+          return res.status(200).json({sucess: true, ddata: []})
+    }
+    res.status(200).json(sortedProducts)
+
+    res.send('Hello World')
    
 })
 
