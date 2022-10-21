c80dd0cf56832b67464b612398610519c5a98198
bochra-mayara <bochra-mayara>
2022-10-21 21:54:50 +0200

    this page

---
+++ auth.js
@@ -0,0 +1,13 @@
+const auth = (req, res, next) => {
+    
+
+    const { user } = req.query
+    if( user === 'John') {
+        req.user = { name: 'John' , id:3}
+        next()
+    }else{
+        res.status(401).send("Unzuthorized")
+    }
+        
+}
+module.exports = auth
